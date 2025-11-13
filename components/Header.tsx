594603821a4c6989a02b1d77f303f95cfd2b44be
pratapcoder <prakashsingh03102>
2025-11-13 23:00:40 +0530

    header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import { ModeToggle } from './ModeToggle'
+import { Button } from './ui/button'
 
 export default function Header() {
   return (
-    <div>
-      <ModeToggle />
+    <div className='flex justify-between bg-green-500 p-4'>
+
+        
+            <h1 className='text-3xl'>AI Explainer</h1>
+
+        
+        <div className='flex gap-4 items-center'>
+            <ModeToggle />
+            <Button variant={"secondary"}>Login</Button>
+           
+        </div>
       
+
     </div>
   )
 }
